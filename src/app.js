19f65b1905e999eaa9295c5835e4b069a3215d21
Rubén Ramírez <rruben084@gmail.com>
2025-01-28 04:10:57 +0000

    Proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function indiceAleatorio(longitud) {
+    return Math.floor(Math.random() * longitud);
+  }
+
+  function generadorMensaje() {
+    let quienCulpable = who[indiceAleatorio(who.length)];
+    let queHizo = action[indiceAleatorio(action.length)];
+    let quieresEvitar = what[indiceAleatorio(what.length)];
+    let cuando = when[indiceAleatorio(when.length)];
+
+    return quienCulpable + " " + queHizo + " " + quieresEvitar + " " + cuando;
+  }
+  let mensajeExcusa = generadorMensaje();
+  document.getElementById("excusa").innerText = mensajeExcusa;
 };
